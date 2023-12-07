fca67239596edf3e6cd9002cc26a2081c2d50331
Alex D <152988659+lodger-close@users.noreply.github.com>
2023-12-07 12:28:21 +0700

    Create VowelManager.cs

---
+++ VowelManager.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class VowelManager : NamePart
+{
+    public VowelManager()
+    {
+        elements = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+    }
+}
