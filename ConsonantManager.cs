3f9fc0ecf08f9d560a1f679b2d4fc59519e9660a
Alex D <152988659+lodger-close@users.noreply.github.com>
2023-12-07 12:28:32 +0700

    Create ConsonantManager.cs

---
+++ ConsonantManager.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class ConsonantManager : NamePart
+{
+    public ConsonantManager()
+    {
+        elements = new List<char> { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+    }
+}
